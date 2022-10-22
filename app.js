8430b90d0fcf5dbed4c503a34ba65f9d35158c13
morpole <morpole@gmail.com>
2022-10-22 16:14:57 +0100

    added a little more code

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-//using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach( function(btn) {
-    btn.addEventListener('click', function(e) {
-        const question = e.currentTarget.parentElement.parentElement
-        question.classList.toggle('show-text');
-    })
-}); 
+// btns.forEach( function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text');
+//     })
+// }); 
+
+
+
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+//console.log(questions)
+
+questions.forEach(function(orange){
+    //console.log(orange);
+    const btn = orange.querySelector('.question-btn');
+    // console.log(btn);
+});
