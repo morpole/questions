362e41abe818915c31e4068ae734bf51c19955d6
morpole <morpole@gmail.com>
2022-10-24 15:55:45 +0100

    finsihed logic with two options

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 // traversing the dom
 
-// const btns = document.querySelectorAll('.question-btn');
+//  const btns = document.querySelectorAll('.question-btn');
 
-// btns.forEach( function(btn) {
-//     btn.addEventListener('click', function(e) {
-//         const question = e.currentTarget.parentElement.parentElement
-//         question.classList.toggle('show-text');
+//  btns.forEach( function(btn) {
+//      btn.addEventListener('click', function(e) {
+//          const question = e.currentTarget.parentElement.parentElement
+//          question.classList.toggle('show-text');
 //     })
-// }); 
+//  }); 
 
 
 
-//using selectors inside the element
+// using selectors inside the element
 
 const questions = document.querySelectorAll('.question');
-//console.log(questions)
+// console.log(questions)
 
 questions.forEach(function(orange){
-    //console.log(orange);
+    // console.log(orange);
     const btn = orange.querySelector('.question-btn');
     // console.log(btn);
+    btn.addEventListener('click', function() {
+
+        questions.forEach(function(bananas){
+            if(bananas !== orange){
+                bananas.classList.remove('show-text')
+            }
+        });
+        orange.classList.toggle('show-text');
+    })
 });
